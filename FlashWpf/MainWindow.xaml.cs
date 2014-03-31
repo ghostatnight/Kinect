08a841efda636d40a25650f41140d9ddbad5a779
ghostatnight <252064973@qq.com>
2014-03-31 18:26:28 +0800

    sln

---
+++ FlashWpf/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms.Integration;
+using AxShockwaveFlashObjects;
+
+namespace FlashWpf
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WindowsFormsHost formHost = new WindowsFormsHost();
+
+            AxShockwaveFlash axShockwaveFlash = new AxShockwaveFlash();
+
+            formHost.Child = axShockwaveFlash;
+
+            mainGrid.Children.Add(formHost);
+
+            string flashPath = Environment.CurrentDirectory;
+            flashPath += @"\game.swf";
+
+            axShockwaveFlash.Movie = flashPath;
+
+        }
+    }
+}
---
+++ WpfFlashControlLibrary1/UserControl1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AxShockwaveFlashObjects;
+using System.Windows.Forms.Integration;
+
+namespace WpfFlashControlLibrary1
+{
+    /// <summary>
+    /// UserControl1.xaml 的交互逻辑
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void flashGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Load flash game
+            WindowsFormsHost formHost = new WindowsFormsHost();
+            AxShockwaveFlash axShockwaveFlash = new AxShockwaveFlash();
+
+            formHost.Child = axShockwaveFlash;
+            flashGrid.Children.Add(formHost);
+
+            string flashPath = Environment.CurrentDirectory;
+            flashPath += @"\game.swf";
+            axShockwaveFlash.Movie = flashPath;
+            axShockwaveFlash.Play();
+
+        }
+    }
+}
